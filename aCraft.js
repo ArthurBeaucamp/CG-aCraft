d2e997c446bfb70d7cf0fb64341567c1435e8a0b
ArthurBeaucamp <arthur_beaucamp@laposte.net>
2018-11-03 21:56:11 +0100

    init

---
+++ aCraft.js
@@ -0,0 +1,20 @@
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+
+for (let i = 0; i < 10; i++) {
+    const line = readline();
+}
+const robotCount = parseInt(readline());
+for (let i = 0; i < robotCount; i++) {
+    var inputs = readline().split(' ');
+    const x = parseInt(inputs[0]);
+    const y = parseInt(inputs[1]);
+    const direction = inputs[2];
+}
+
+// Write an action using print()
+// To debug: printErr('Debug messages...');
+
+print('0 0 U 1 1 R 2 2 D 3 3 L');
